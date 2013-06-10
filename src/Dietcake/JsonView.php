bd91e9a32c26ab35a281d25922b38632c36d195e
xcezx <main.xcezx@gmail.com>
2013-06-10 18:42:07 +0900

    init

---
+++ src/Dietcake/JsonView.php
@@ -0,0 +1,9 @@
+<?php
+class JsonView extends View
+{
+    public function render($action = null)
+    {
+        header('Content-Type: application/json; charset=utf-8');
+        $this->controller->output .= json_encode($this->vars);
+    }
+}
